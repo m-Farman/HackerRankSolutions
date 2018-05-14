751c46e2e2174c783e804fb9d588ac9ceb278441
m-Farman <farmanmohd8@gmail.com>
2018-05-15 00:26:54 +0530

    adding Alternative Queries soln

--- SQL/06_Alternative Queries/03_Print Prime Numbers/Print Prime Numbers.sql
+++ SQL/06_Alternative Queries/03_Print Prime Numbers/Print Prime Numbers.sql
@@ -2,10 +2,8 @@
 select listagg(r, '&' ) within group (order by r) 
 from (
 with ct as (select rownum+1 as r from all_objects where rownum < 1000)
-select r , 1 as rnk
+select r 
 from ct o
 where r <= 1000
 and not exists (select mod(o.r,i.r) from ct i  where i.r <= o.r/2 and mod(o.r,i.r)=0)
-)
- group by rnk;
-
+);

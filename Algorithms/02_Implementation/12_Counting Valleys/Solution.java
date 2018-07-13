9346f215b1fecdd61bb24740035c95a4354ed682
m-Farman <farmanmohd8@gmail.com>
2018-07-14 01:24:00 +0530

    More soln

---
+++ Algorithms/02_Implementation/12_Counting Valleys/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int height =0 ,valley =0 , tmp =0;
+        for(int i=0;i<s.length();i++){
+            char c =s.charAt(i);
+            tmp = (c=='U')? 1: -1;
+            if( height < 0 && (tmp + height) ==0){
+                valley ++;
+            }
+            height += tmp;
+        }
+    return valley;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+

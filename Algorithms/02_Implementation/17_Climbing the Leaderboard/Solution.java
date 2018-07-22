ab30bffd87bea237171a9f5af453a9363c75c8f2
m-Farman <farmanmohd8@gmail.com>
2018-07-22 23:42:44 +0530

    17_Climbing the Leaderboard

---
+++ Algorithms/02_Implementation/17_Climbing the Leaderboard/Solution.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the climbingLeaderboard function below.
+    static int[] climbingLeaderboard(int[] scores, int[] alice) {
+       int[] score = new int[scores.length];
+         int index =0;
+            for (int i = 0; i < scores.length; i++){
+                if (index == 0 || scores[i] != score[index-1])
+                    score[index++]=scores[i];
+            }
+
+
+        int prevIndex = index-1;
+        int[] output=new int[alice.length];
+        for (int i = 0; i < alice.length; i++) {
+                int s = alice[i];
+                int min = 0;
+                int max = index;
+                while (max > min){
+                    int mid = (min + max) / 2;
+                    if (score[mid] <= s)
+                        max = mid;
+                    else
+                        min = mid + 1;
+                }
+            output[i] = min+1;
+        }
+    return output;
+    }
+
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int scoresCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[scoresCount];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < scoresCount; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int aliceCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] alice = new int[aliceCount];
+
+        String[] aliceItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < aliceCount; i++) {
+            int aliceItem = Integer.parseInt(aliceItems[i]);
+            alice[i] = aliceItem;
+        }
+
+        int[] result = climbingLeaderboard(scores, alice);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+

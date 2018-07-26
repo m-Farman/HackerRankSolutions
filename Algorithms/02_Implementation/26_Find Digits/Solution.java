4ef02a03b8e5c52bbe4593facf9b55c5ab18f5e5
m-Farman <farmanmohd8@gmail.com>
2018-07-26 22:55:38 +0530

    Find Digits

---
+++ Algorithms/02_Implementation/26_Find Digits/Solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+
+        int n2 =n;
+        int divisor = 0, d =0;
+        while(n2>0){
+            d= n2%10;
+            if(d!=0){
+                divisor += (n%d == 0)?1:0;
+            }
+            n2 /=10;
+        }
+        return divisor;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+

a4dd7e54c1b6f3c548bc9af537a893e18e7c1646
m-Farman <farmanmohd8@gmail.com>
2018-09-23 00:15:48 +0530

    String completed

--- Java/02_String/08_Java Regex/Solution.java
+++ Java/02_String/08_Java Regex/Solution.java
@@ -1,6 +1,21 @@
 //Write your code here
 
 class MyRegex{
+/*
+	^		#start of the line
+	 (		#  start of group #1
+	   [01]?\\d\\d? #    Can be one or two digits. If three digits appear, it must start either 0 or 1
+			#    e.g ([0-9], [0-9][0-9],[0-1][0-9][0-9])
+	    |		#    ...or
+	   2[0-4]\\d	#    start with 2, follow by 0-4 and end with any digit (2[0-4][0-9]) 
+	    |           #    ...or
+	   25[0-5]      #    start with 2, follow by 5 and ends with 0-5 (25[0-5]) 
+	 )		#  end of group #2
+	  \.            #  follow by a dot "."
+	....            # repeat with 3 times (3x)
+	$		#end of the line
+*/
+
     String pattern="^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
         "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
         "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +

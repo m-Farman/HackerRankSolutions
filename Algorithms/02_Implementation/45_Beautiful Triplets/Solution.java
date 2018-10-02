ae927e62017a85aaff8fd25cdc22e83f174debc1
m-Farman <farmanmohd8@gmail.com>
2018-10-03 01:37:33 +0530

    Beautiful Triplets

---
+++ Algorithms/02_Implementation/45_Beautiful Triplets/Solution.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulTriplets function below.
+    static int beautifulTriplets(int d, int[] arr) {
+
+        HashMap<Integer, Integer> numbers = new HashMap<>();
+        int totalTriplet = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (numbers.containsKey(arr[i])) {
+                numbers.put(arr[i], numbers.get(arr[i]) + 1);
+            } else {
+                numbers.put(arr[i], 1);
+            }
+
+            if (numbers.containsKey(arr[i] - d)) {
+                if (numbers.containsKey(arr[i] - 2*d)) {
+                    totalTriplet += numbers.get(arr[i] - d) * numbers.get(arr[i] - 2 * d);
+                }
+            }
+        }
+        
+        return totalTriplet;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = beautifulTriplets(d, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+

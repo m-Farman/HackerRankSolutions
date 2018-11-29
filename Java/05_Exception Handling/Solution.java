c3cc8bafcc90a9ab4ff7c0e5dc9fc7e37e5ebea6
m-Farman <farmanmohd8@gmail.com>
2018-11-29 23:40:42 +0530

    Exception handling added

---
+++ Java/05_Exception Handling/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       Scanner scan = new Scanner(System.in);
+        try {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println(a / b);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+

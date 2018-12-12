5fc6d2d362f0c1874a689df33f7a8175ba83c940
m-Farman <farmanmohd8@gmail.com>
2018-12-12 23:40:30 +0530

    Java SHA-256

---
+++ Java/06_Advanced/11_Java SHA-256/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.security.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(str.getBytes());
+            for (byte b : digest) {
+                System.out.printf("%02x", b);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
+
+
